3e12795f9d7da3efd759a86c531da7a031b62fdd
Javed Khan <tuxcanfly@gmail.com>
2014-09-05 15:33:40 +0530

    added lookup call to client

--- rdict.c
+++ rdict.c
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
     char *server;
     char *proc = malloc(sizeof(char) * 128);
     char *word = malloc(sizeof(char) * 128);
+    char *meaning = malloc(sizeof(char) * 128);
     char *insert = "INSERT";
     char *lookup = "LOOKUP";
 
@@ -38,10 +39,32 @@ int main(int argc, char **argv)
                 return 1;
             }
 
+            if (*result == 100) {
+                printf("sucessfully added '%s' to the dictionary\n", word);
+            }
+
             if (*result == 0) {
                 fprintf(stderr, "could not insert %s\n", word);
                 return 1;
             }
+
+            clnt_destroy( clnt );
+        }
+        else if (!strcmp(proc, lookup)) {
+            clnt = clnt_create(server, DICTPROG, DICTVERS, "tcp"); 
+
+            if (clnt == (CLIENT *)NULL) {
+                clnt_pcreateerror(server);
+                return 1;
+            }
+
+            meaning = *lookup_1(&word, clnt);
+            if (result == (int *)NULL) {
+                clnt_perror(clnt, server);
+                return 1;
+            }
+
+            printf("sucessfully got meaning of '%s' from the dictionary: %s\n", word, meaning);
 
             clnt_destroy( clnt );
         }
